8c91a91d7a00c059b247dab61aab94063fa3cc5a
mateoramirezdev <mateo.sebastiansz28@gmail.com>
2025-12-04 19:42:13 -0300

    Update TP 2_Final final.cpp

--- TP 2_Final final.cpp
+++ TP 2_Final final.cpp
@@ -1,279 +1,198 @@
 #include <iostream>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX_CADENA 50
-#define CANT_GENEROS 20
-#define MAX_AUTORES 100
-
 using namespace std;
 
-struct Libro {
-    char genero[MAX_CADENA];
-    int cod_genero;
-    char titulo[MAX_CADENA];
-    char autor[MAX_CADENA];
-    int paginas;
-};
 
-struct NodoLibro {
-    Libro libro;
-    NodoLibro* sig;
-};
-struct Autor{  
-	char nombre[MAX_CADENA];
-	int cantidadLibros;
+
+//Dato de las huertas
+struct huertas_registradas {
+	int id_huerta;
+	string barrio;
+	char tipo_cultivo;
+	float sup_cultivada;
+	float prod_mensual;
+	string fertilizante;
 	};
-
-
-void crear_datos() {
-    int cant;
-    cout << "Cuantos libros desea cargar?" << endl;
-    cin >> cant;
-
-    FILE* miArchivo = fopen("ArchivoLibros.dat", "wb");
-    if (!miArchivo) {
-        cout << "Error al abrir archivo" << endl;
-        return;
-    }
-
-    Libro data;
-    for (int i = 0; i < cant; i++) {
-        cout << "Escriba el genero del libro: ";
-        cin >> data.genero;
-        cout << "Escriba el codigo de genero del libro (1-20): ";
-        cin >> data.cod_genero;
-        cout << "Escriba el titulo del libro: ";
-        cin >> data.titulo;
-        cout << "Escriba el autor del libro: ";
-        cin >> data.autor;
-        cout << "Escriba la cantidad de paginas del libro: ";
-        cin >> data.paginas;
-
-        fwrite(&data, sizeof(Libro), 1, miArchivo);
-    }
-    fclose(miArchivo);
-}
-
-NodoLibro* insertarOrdenado(NodoLibro*& p, Libro info) {
-    NodoLibro* nuevo = new NodoLibro();
-    nuevo->libro = info;
-    nuevo->sig = NULL; 
-    
-    NodoLibro* ant = NULL;
-    NodoLibro* aux = p;
- 
-    while (aux != NULL && aux->libro.paginas < info.paginas) {
-        ant = aux;
-        aux = aux->sig;
-    }
-
-    if (ant == NULL) {
-     
-        nuevo->sig = p; 
-        p = nuevo;      
-    } else {
-        
-        ant->sig = nuevo; 
-        nuevo->sig = aux; 
-    }
-    
-
-    return p;
-}
-
-
-void cargarDatos(NodoLibro* vector_generos[]) {
-    FILE* miArchivo = fopen("ArchivoLibros.dat", "rb");
-    if (!miArchivo) {
-        cout << "No se pudo abrir el archivo" << endl;
-        return;
-    }
-	long posicionInicio = ftell(miArchivo);
-    fseek(miArchivo, 0, SEEK_END);
-    long posicionFinal = ftell(miArchivo);
-	int cant_libros = (posicionFinal - posicionInicio) / sizeof(Libro);
-
-    
-    fseek(miArchivo, posicionInicio, SEEK_SET);
-
-    Libro data;
-    for (int i = 0; i < cant_libros; i++) {
-        if (fread(&data, sizeof(Libro), 1, miArchivo) == 1) {
-            
-            
-                insertarOrdenado(vector_generos[data.cod_genero - 1], data);
-        }
-        }
-    
-
-    fclose(miArchivo);
-}
-
-
-void encontrarAutorFrecuente(NodoLibro* vector_generos[]){
-	char autorFrecuente[MAX_CADENA];
-
-	Autor autores[MAX_AUTORES]; 
-	int cantAutores = 0; 
-	int encontrado = 0;
 	
-	for (int i = 0; i < CANT_GENEROS; i++) {
-        NodoLibro* actual = vector_generos[i];
-        while (actual != NULL) {
-       		int encontrado = 0;
-			for (int j = 0; j < cantAutores; j++) {
-    			if (strcmp(autores[j].nombre, actual->libro.autor) == 0) { 
-        			autores[j].cantidadLibros++; 
-        			encontrado = 1;  
-  	  			}
-			}	
+char mas_frecuente(huertas_registradas array[], int cant);
 
-			if (!encontrado && cantAutores < MAX_AUTORES) { 
-				strcpy(autores[cantAutores].nombre, actual->libro.autor);  
-				autores[cantAutores].cantidadLibros = 1; 
-    			cantAutores++; 
-    		}
- 			actual = actual->sig; 
-	 	}
+//Funcion para cargar los datos en mi array de huertas
+void carga_datos(huertas_registradas array [], int cant_huertas){
+	for (int i=0; i<cant_huertas; i++){
+		cout<<"Ingrese el identificador de la huerta"<<endl;
+		cin>>array[i].id_huerta;
+		cout<<"Ingrese el barrio"<<endl;
+		cin>>array[i].barrio;
+		cout<<"Ingrese el tipo de cultivo"<<endl;
+		cin>>array[i].tipo_cultivo;
+		cout<<"Ingrese la superficie cultivada"<<endl;
+		cin>>array[i].sup_cultivada;
+		cout<<"Ingrese la produccion mensual"<<endl;
+		cin>>array[i].prod_mensual;
+		cout<<"Ingrese si utlizo fertilizante"<<endl;
+		cin>>array[i].fertilizante;
 	}
-	
-	int maxAutor = 0; 
-    	for (int i = 1; i < cantAutores; i++) { 
-        	if (autores[i].cantidadLibros > autores[maxAutor].cantidadLibros) { 
-           		maxAutor = i;
-        	}
-    	}
-    
-	cout<<"Autor más frecuente: "<<autores[maxAutor].nombre<<" con "<<autores[maxAutor].cantidadLibros<<" libros."<<endl; 
-};
-
-
-void tituloMaxPaginas(NodoLibro* vector_generos[]){
-	int maxPaginas = 0;
-	char tituloMasPaginas[MAX_CADENA];
-	for (int i = 0; i < CANT_GENEROS; i++) { 
-        NodoLibro* actual = vector_generos[i];
-        while (actual != NULL) { 
-   			if (actual->libro.paginas > maxPaginas) {  
-        		maxPaginas = actual->libro.paginas; 
-        		strcpy(tituloMasPaginas, actual->libro.titulo);  
-   			}
-    		actual = actual->sig; 
+}
+//Muestro  la cantidad de huertas registradas, el promedio mensual de produccion y de superficie cultivda, el porcentaje de fertilizados
+// y el tipo de cultivo mas frecuente
+void mostrar (huertas_registradas array[], int cant ){
+	float suma=0;
+	for (int j=0; j<cant; j++){
+		suma+=array[j].prod_mensual;
+	}
+	float promedio=suma/cant;
+	float suma_sup=0;
+	for (int l=0; l<cant; l++){
+		suma_sup+=array[l].sup_cultivada;
+	}
+	float promedio_sup=suma_sup/cant;
+	int fertilizantes=0;
+	int porcentaje;
+	for (int k=0; k<cant; k++){
+		if (array[k].fertilizante=="Si"){
+			fertilizantes++;
 		}
 	}
-    cout<<"Libro con mayor cantidad de paginas: "<<tituloMasPaginas<<" con "<<maxPaginas<<" páginas."<<endl; 
-};
-
-void paginas(NodoLibro* vector_generos[]) {
-    int totalLibros = 0;
-    int sumaPaginas = 0;
-
-    
-    for (int i = 0; i < CANT_GENEROS; i++) {
-        NodoLibro* aux = vector_generos[i];
-        while (aux != NULL) {
-            totalLibros++;
-            sumaPaginas += aux->libro.paginas;
-            aux = aux->sig;
-        }
-    }
-
-    cout << "\n RESULTADOS" << endl;
-    cout << "Cantidad total de libros registrados: " << totalLibros << endl;
-
-    
-    if (totalLibros > 0) {
-        int promedio = sumaPaginas / totalLibros;
-        cout << "Promedio de paginas por libro: " << promedio << endl;
-    } else {
-        cout << "No hay libros cargados." << endl;
-        return; 
-    }
-}
-
-void contarPorGenero(NodoLibro* vector_generos[]) {
-    int maxLibros = 0;
-    int generoMax = -1;
-
-    for (int i = 0; i < CANT_GENEROS; i++) {
-        int contador = 0;
-        NodoLibro* aux = vector_generos[i];
-
-   
-        while (aux != NULL) {
-            contador++;
-            aux = aux->sig;
-        }
-
-        cout << "Genero " << (i + 1) << " -> " << contador << " libros" << endl;
-
-       
-        
+	porcentaje=(fertilizantes*100)/cant;
+	
+	
+		cout<<"Cantidad de huertas registradas:  "<<cant<<endl;
+		cout<< "Produccion mensual promedio"<<promedio<<endl;
+		cout<<"Porcentaje de huertas que usan fertilizante"<<porcentaje<<"%"<<endl;
+		cout<<"Promedio de superficie cultivada"<<promedio_sup<<endl;
+		cout<<"Tipo de cultivo mas frecuente: "<<mas_frecuente (array, cant)<<endl;
 		
-		 
-        if (contador > maxLibros) {
-            maxLibros = contador;
-            generoMax = i + 1;  
-    	}
-    }
-
-    if (generoMax != -1) {
-        cout << "\nEl genero con mas libros es el " << generoMax 
-             << " con " << maxLibros << " libros." << endl;
-    } else {
-        cout << "No hay libros cargados." << endl;
-    }
-}
-
-void mostrarResultados (NodoLibro*vector_generos[]){
-	        contarPorGenero(vector_generos);
-			paginas(vector_generos);       
-			encontrarAutorFrecuente (vector_generos);
-			tituloMaxPaginas(vector_generos);
+	
 }
 
 
-int main() {
+//Me tira el tipo de cultivo mas frecuente
+char mas_frecuente(huertas_registradas array[], int cant) {
+    int cH = 0, cF = 0, cA = 0, cL = 0, cO = 0;
 
-    NodoLibro* vector_generos[CANT_GENEROS]={NULL};
+    for (int i = 0; i < cant; i++) {
+     		switch(array[i].tipo_cultivo){
+        	case'H': cH++;
+        	break;
+        	case'F': cF++;
+        	break;
+        	case'A': cA++;
+        	break;
+        	case'L': cL++;
+        	break;
+        	case'O': cO++;
+        	break;
+        }
+    }
 
-    
-    cargarDatos(vector_generos); 
+    // Comparar para obtener el tipo con mayor cantidad
+    int mayor = cH;
+    char tipo = 'H';
 
-    int opcion = 0;
-    string respuesta;
+    if (cF > mayor) {
+        mayor = cF;
+        tipo = 'F';
+    }
+    if (cA > mayor) {
+        mayor = cA;
+        tipo = 'A';
+    }
+    if (cL > mayor) {
+        mayor = cL;
+        tipo = 'L';
+    }
+    if (cO > mayor) {
+        mayor = cO;
+        tipo = 'O';
+    }
 
-    while (opcion != 3) {
-        cout << "\n--- MENU ---" << endl;
-        cout << "1) Cargar Datos" << endl;
-        cout << "2) Mostrar Resultados" << endl;
-        cout << "3) Salir" << endl;
-        cin >> opcion;
+    return tipo;
+}
 
-        if (opcion == 1) {
-            
 
-            for (int i = 0; i < CANT_GENEROS; i++) {
-                vector_generos[i] = NULL; 
-            }
-            
-            cout << "Desea SOBREESCRIBIR el archivo con nuevos libros? (Si/No): ";
-            cin >> respuesta;
-            
-            if (respuesta == "Si" || respuesta == "si") {
-
-                crear_datos(); 
-            } 
-
-            cargarDatos(vector_generos);
-            cout << "\nDatos leídos/actualizados desde el archivo a la memoria correctamente." << endl;
-            
-        } else if (opcion == 2) {
-           mostrarResultados (vector_generos);
+//Me muestra la cantidad de cultivos por barrio
+void Barrios (huertas_registradas arrayh[], int cant_pb [], string arrayb [], int canth, int cantb){
+	for(int i=0; i<canth; i++){
+		for (int j=0;j<cantb; j++){
+			if (arrayh[i].barrio==arrayb[j]){
+				cant_pb[j]++;
+			}
 		}
-    }
+	}
+	for (int k=0; k<cantb; k++){
+		int d=cant_pb[k];
+		if(d!=0){
+		
+		cout<<"Cantidad de huertas en "<<arrayb[k]<<" es de "<<cant_pb[k]<<endl;
+	}
+	}}
+	
 
-    return 0;
-} 
+void menu(){
+	
+	cout<<"Seleccione una opcion"<<endl;
+	cout<<"1) Cargar y procesar datos de huerta"<<endl;
+	cout<<"2) Mostrar resultados"<<endl;
+	cout<<"3) Salir"<<endl;
+
+}
+
+
+
+int main(int argc, char** argv) {
+	const int MAX=100;
+	huertas_registradas a_huertas [MAX];
+	int respuesta;
+	int cant_huertas=0;
+	
+	int cant_por_barrios [48]{0};
+	string barrios [48]{"Agronomia", "Almagro", "Balvanera",
+	"Barracas", "Belgrano", "Boedo", "Caballito", "Chacarita", "Coghlan",
+	"Colegiales", "Constitucion", "Flores", "Floresta", "La Boca", "La Paternal",
+	"Liniers", "Mataderos", "Monte Castro", "Monserrat", "Nueva Pompeya", "Nunez", "Palermo",
+	"Parque Avellaneda", "Parque Chacabuco", "Parque Chas", "Parque Patricios", "Puerto Madero",
+	"Recoleta", "Retiro", "Saveedra", "San Cristobal", "San Nicolas", "San Telmo",
+	"Velez Sarsfield", "Versalles", "Villa Crespo", "Villa del Parque",
+	"Villa Devoto", "Villa General Mitre", "Villa Lugano", "Villa Luro",
+	"Villa Ortuzar", "Villa Pueyrredon", "Villa Real", "Villa Riachuelo",
+	"Villa Santa Rita", "Villa Soldati", "Villa Urquiza"};
+	cout<<"Bienvenido"<<endl;
+	while(respuesta!=3){
+	menu();
+	cin>>respuesta;
+		if (respuesta==1){
+			cout<<" Cuantas huertas desea cargar?"<<endl;
+			cin>>cant_huertas;
+		
+			carga_datos (a_huertas, cant_huertas);
+			string res2;
+			cout<<"Desea ver los resultados?"<<endl;
+			cin>>res2;
+			if (res2=="Si"){
+			mostrar (a_huertas, cant_huertas);
+			Barrios (a_huertas, cant_por_barrios, barrios, cant_huertas, 48);
+			cout<< "Tipo de cultivo mas frecuente"<<mas_frecuente  (a_huertas, cant_huertas)<<endl;
+		
+		 }
+		}
+		if(respuesta==2 ){
+			if(cant_huertas==0){
+			
+			cout << "No se han cargado datos de huertas" << endl;
+			cout  << "Puedes cerrar el programa o volver al menu" << endl;
+			cout << " Cerrar el programa=0 | Volver al menu=1" << endl;
+			int cerrar;
+			cin >> cerrar;
+			if(cerrar==0) return 0;}
+			
+			else {
+      		mostrar(a_huertas, cant_huertas);
+     		Barrios(a_huertas, cant_por_barrios, barrios, cant_huertas, 48);
+        	
+    }
+			}
+		}	return 0;
+	}
+
+
+
+
